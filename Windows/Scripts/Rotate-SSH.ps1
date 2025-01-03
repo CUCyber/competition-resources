8a0eb49d900dd6efad3a3f0514a51256efedf263
D-Guy Desktop <ddcool535@gmail.com>
2025-01-03 15:17:15 -0500

    Updated Rotate-SSH script, confirmed working successfully

--- Windows/Scripts/Rotate-SSH.ps1
+++ Windows/Scripts/Rotate-SSH.ps1
@@ -1,16 +1,9 @@
 # Author: Dylan Harvey
-# Script for rotating SSH keys. (WIP v0.1)
-# TODO: All Users, File Permissions?
+# Script for rotating SSH keys for the current user.
 
 $CurrentDir = Get-Location
 $NewPubKeyPath = "$CurrentDir\cyberKey.pub"
 $AuthKeysPath = "$env:USERPROFILE\.ssh\authorized_keys"
-
-# Ensure authorized_keys exists
-if (-Not (Test-Path $AuthKeysPath)) {
-    New-Item -ItemType File -Path $AuthKeysPath -Force | Out-Null
-    Write-Host "No authorized_keys found. Created a new one at $AuthKeysPath"
-}
 
 # Read new public key
 if (Test-Path $NewPubKeyPath) {
@@ -21,11 +14,14 @@ if (Test-Path $NewPubKeyPath) {
     return
 }
 
+# Ensure authorized_keys exists
+if (-Not (Test-Path $AuthKeysPath)) {
+    New-Item -ItemType File -Path $AuthKeysPath -Force | Out-Null
+    Write-Host "No authorized_keys found. Created a new one at $AuthKeysPath"
+}
+
 # Removes all old keys and adds new key
 Set-Content -Path $AuthKeysPath -Value $NewPubKey
 Write-Host "authorized_keys cleared and updated."
-
-# Update permissions on authorized_keys file?
-# icacls?
 
 Pause
