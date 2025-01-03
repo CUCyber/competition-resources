05705485954672755cb96317b0aefad6df80810c
D-Guy Desktop <ddcool535@gmail.com>
2025-01-03 14:52:24 -0500

    Updated Nuke Task Scheduler Script, works but may require PSExec for system permissions

--- Windows/Scripts/Nuke-Task-Scheduler.ps1
+++ Windows/Scripts/Nuke-Task-Scheduler.ps1
@@ -1,14 +1,20 @@
 # Author: Dylan Harvey
-# Script for nuking task scheduler off the map. (WIP)
+# Script for removing task scheduler in its entirety.
 
+# NOTE: May require PSExec even as Administrator due to registry access
 #Requires -RunAsAdministrator
 
 # Remove all scheduled tasks
-Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tasks" -Recurse -Force
+Write-Host "Removing scheduled tasks..." -ForegroundColor Yellow
+Remove-Item -Path "$RegPath\Tasks" -Recurse -Force # -ErrorAction SilentlyContinue
 
 # Restart the Task Scheduler service
-Restart-Service -Name Schedule
+Write-Host "Restarting Task Scheduler service..." -ForegroundColor Yellow
+Stop-Service -Name Schedule -Force
+Start-Service -Name Schedule
 
-# Kill task processes (Restart Should handle this; precautionary)
+# Kill task processes (Restart-Service Should handle this; precautionary)
+Write-Host "Killing task-related processes..." -ForegroundColor Yellow
 Get-Process -Name taskhostw | Stop-Process -Force
 
+Pause
