7d878c8b297b8d78aaa764e29a9cf430cdaa99ae
D-Guy Desktop <ddcool535@gmail.com>
2025-01-06 14:09:56 -0500

    Updated Inital password reset script to have CSV output.

--- Windows/Scripts/Initial-PWReset-AD.ps1
+++ Windows/Scripts/Initial-PWReset-AD.ps1
@@ -4,6 +4,14 @@
 $safeMode = $true
 $excludedUsers = @("krbtgt", "gold-team", "scoring")
 $defaultPassword = "NewSecurePassword123!"
+$outputFile = "affectedUsers.csv"
+
+if (Test-Path $outputFile) {
+    Remove-Item $outputFile -Force
+}
+if ($safeMode) {
+    Add-Content -Path $outputFile -Value "SAFE MODE ENABLED; NO CHANGES MADE"
+}
 
 $allUsers = Get-ADUser -Filter * -Properties SamAccountName
 
@@ -19,10 +27,11 @@ foreach ($user in $allUsers) {
         Set-ADAccountPassword -Identity $user.SamAccountName -NewPassword (ConvertTo-SecureString -AsPlainText $defaultPassword -Force)
         Write-Output "Password reset for: $($user.SamAccountName)"
     }
+    Add-Content -Path $outputFile -Value "$($user.SamAccountName)::$defaultPassword"
 }
 
 if ($safeMode) {
-    Write-Output "Safe mode is ON. No passwords were changed."
+    Write-Output "Safe mode is ON. No passwords were changed. Expected changes written to: $outputFile"
 } else {
-    Write-Output "Safe mode is OFF. Passwords have been reset for all applicable users."
+    Write-Output "Safe mode is OFF. Passwords have been reset for all applicable users. Changes written to: $outputFile"
 }
