01a44812f805586abd62486d3a10e80dea30ddd9
isaac-fletcher <ifletch@clemson.edu>
2025-02-19 10:48:41 -0500

    Updated linux scripts

--- Linux/Scripts/ss-kill+fzf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ss -tulpan | ./fzf --preview-window=right,30% --multi --header-lines=1 --reverse --preview='echo {} | sed '"'"'s/.*pid=\([0-9]\+\).*/\1/g'"'"' | xargs ps h -o cmd -p' | sed 's/.*pid=\([0-9]\+\).*/\1/g' | xargs kill -9
