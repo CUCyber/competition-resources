b48978c524ac088110ec1b6f5074a247daa5e03d
Laptop D-Guy <ddcool535@gmail.com>
2025-04-14 18:50:11 -0400

    Added empty password check and exclusion warning

--- Windows/Scripts/Reset-Passwords.ps1
+++ Windows/Scripts/Reset-Passwords.ps1
@@ -2,20 +2,29 @@
 # Author: Dylan Harvey
 # Manual password reset script, will change passwords for non-excluded user accounts.
 
-$excludedUsers = @("krbtgt", "ansible", "blackteam_adm", "^seccdc") # CHANGE AS NEEDED, SUPPORTS REGEX
-$password = "NewSecurePassword123!"
+$excludedUsers = @("krbtgt", "ansible", "^seccdc") # CHANGE AS NEEDED, SUPPORTS REGEX
+$password = "" # CHANGE
+if (!$password) {
+    Write-Host "Password is not set! Aborting..." -ForegroundColor Red
+    exit 2
+} elseif (!$excludedUsers) {
+    Write-Host "WARNING: Excluded users list is not set!" -ForegroundColor Yellow
+    Start-Sleep 2
+}
 $securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
 $outputFile = ".\affectedUsers.csv"
-Out-File $outputFile # Clear file if exists, creates if not
+Out-File $outputFile # Clear/create file
 
 $role = (Get-WmiObject Win32_ComputerSystem).DomainRole
 if ($role -ge 4) {
     Write-Host "Machine is a Domain Controller." -ForegroundColor Cyan
 
     $allUsers = Get-ADUser -Filter * | Select-Object -ExpandProperty SamAccountName
-    $allUsers = $allUsers | ForEach-Object {
-        if ($_ -match ($excludedUsers -join "|")) { Write-Host "Skipping excluded user: $_" -ForegroundColor Magenta } 
-        else { $_ }
+    if ($excludedUsers) {
+        $allUsers = $allUsers | ForEach-Object {
+            if ($_ -match ($excludedUsers -join "|")) { Write-Host "Skipping excluded user: $_" -ForegroundColor Magenta } 
+            else { $_ }
+        }
     }
 
     $allUsers | ForEach-Object {
@@ -29,9 +38,11 @@ if ($role -ge 4) {
     Write-Host "Machine is NOT a Domain Controller." -ForegroundColor Cyan
 
     $allUsers = Get-LocalUser | Select-Object -ExpandProperty Name
-    $allUsers = $allUsers | ForEach-Object {
-        if ($_ -match ($excludedUsers -join "|")) { Write-Host "Skipping excluded user: $_" -ForegroundColor Magenta } 
-        else { $_ }
+    if ($excludedUsers) {
+        $allUsers = $allUsers | Where-Object {
+            if ($_ -match ($excludedUsers -join "|")) { Write-Host "Skipping excluded user: $_" -ForegroundColor Magenta } 
+            else { $_ }
+        }
     }
 
     $allUsers | ForEach-Object {
@@ -43,7 +54,7 @@ if ($role -ge 4) {
     Write-Host "Passwords have been reset for local users. Changes written to: $outputFile" -ForegroundColor Green
 } else { # I've never seen this happen but just in case
     Write-Host "Error determining machine type." -ForegroundColor Red
-    exit 2 # Manually envoked exit
+    exit 2
 }
 
 Write-Host "Passwords have been reset for all applicable users. Changes written to: $outputFile" -ForegroundColor Green
